0df273a8018f146af5bb4e11a372dc8017a50efb
SeokHoon <sughoon1@gmail.com>
2024-08-13 19:29:34 +0900

    finish

---
+++ src/component/Box.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Box = (props) => {
+    let result;
+    if(props.title==="COMPUTER" && props.result !== "TIE" && props.result !==""){
+        result = props.result === "WIN" ? "LOSE" : "WIN";
+    }else {
+        result = props.result;
+    }
+    return (
+        <div className={`box ${result.toLowerCase()}`}>
+            <h1>{props.title}</h1>
+            <img className="item-img" src={props.item && props.item.img}/>
+            <h2>{result}</h2>
+        </div>
+    );
+};
+
+export default Box;
